d84834ed89c1d8c18b1445db3efbc5cf0021be9d
ASUS <ASUS@DESKTOP-NB7MT4D>
2024-06-19 12:27:59 +0400

    asd

---
+++ 10. Linq Methods Task/Class Extention.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static empty.Program;
+
+namespace _10._Linq_Methods_Task
+{
+    public static class Class_Extention
+    {
+        public static int method(this int a) {
+            return a  =  a<0 ? a:a*(-a) ;
+        } 
+    }
+}
